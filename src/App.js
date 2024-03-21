879623aa241d188b9036fa5e80732599f68b0350
Sarah.Keller <sarah29.84@gmail.com>
2024-03-21 14:08:27 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import { useState } from 'react';
+
+function GoalForm(props) {
+  const [formData, setFormData] = useState({ goal: '', by: '' });
+
+  // [e.target.name] allows dynamic selecting, eg goal or by
+  function changeHandler(e) {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  }
+
+  function submitHandler(e) {
+    e.preventDefault();
+    props.onAdd(formData);
+    setFormData({ goal: '', by: '' });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>My goals</h1>
+      <form onSubmit={submitHandler}>
+        <input
+          type="text"
+          name="goal"
+          placeholder="goal"
+          value={formData.goal}
+          onChange={changeHandler}
+        />
+        <input
+          type="text"
+          name="by"
+          placeholder="by"
+          value={formData.by}
+          onChange={changeHandler}
+        />
+        <button>Submit goal</button>
+      </form>
+    </>
+  );
+}
+
+function ListOfGoals(props) {
+  return (
+    <ul>
+      {props.allGoals.map((g) => (
+        <li key={g.goal}>
+          <span>
+            My goal is to {g.goal}, by {g.by}
+          </span>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+function App() {
+  const [allGoals, updateAllGoals] = useState([]);
+
+  function addGoal(goal) {
+    updateAllGoals([...allGoals, goal]);
+  }
+
+  return (
+    <>
+      <GoalForm onAdd={addGoal} />
+      <ListOfGoals allGoals={allGoals} />
+    </>
   );
 }
 
